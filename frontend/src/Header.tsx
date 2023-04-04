616e7cf67f82367081b8f1e4bffad8d301144f67
Riley Marshall <rileyrm@byu.edu>
2023-04-04 15:13:32 -0600

    Header on the front end done

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="row">
+      <div className="row-4">Joel Hilton Movie Collection</div>
+    </header>
+  );
+}
+
+export default Header;
--- frontend/src/main/App.tsx
+++ frontend/src/main/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "../movieLogo1.jpeg";
+import "./App.css";
+import Header from "../Header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
